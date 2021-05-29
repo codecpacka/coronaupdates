b6ace8fa6728aefaf719d0c1090aa592aa882def
BhargavSankaliya <sakaliyabhargav@gmail.com>
2021-05-29 14:50:45 +0530

    error in coad

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,16 @@
+const URL = "https://covid19.mathdro.id/api";
+
 let app = angular.module("MyApp",[]);
 
 app.controller("MyCtrl", ($scope) => {
-    $scope.title = "Covid-19";
+    $scope.title = "Stay Home Stay Safe";
+
+    console.log("App Loaded");
+
+    fetch('https://covid19.mathdro.id/api')
+    .then((response) => response.json()
+    .then((data) => {
+        console.log (response.data);
+        $scope.all_data = response.data;
+    })
 });
