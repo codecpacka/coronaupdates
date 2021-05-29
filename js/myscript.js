859d202eab59cf3a55f58d8c275885ea1ba999f3
BhargavSankaliya <sakaliyabhargav@gmail.com>
2021-05-29 16:13:40 +0530

    added data

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,38 @@
+const URL ="https://covid19.mathdro.id/api";
+
 let app = angular.module("MyApp", []);
 
-app.controller("MyCtrl", ($scope) => {
+app.controller("MyCtrl", ($scope, $http) => {
   $scope.title = "Stay Home Stay Safe";
 
   console.log("App Loaded");
 
-  fetch("https://covid19.mathdro.id/api")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      $scope.all_data = data;
-    });
+  $http.get(URL).then(
+    (response) => {
+      console.log(response.data);
+      $scope.all_data = response.data;
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+  $scope.get_c_data = () => {
+    let country = $scope.c;
+    if(country == '')
+    {
+      return;
+    }
+
+    $http.get('${URL}/countries/${country}')
+    .then(
+      (response) => {
+        console.log(response.data);
+        $scope.c_data = response.data;
+      }, 
+      (error) => {
+        console.log(error);
+      });
+
+  };
 });
