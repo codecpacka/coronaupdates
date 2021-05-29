7f1008995e2a8897781e199d8bdd35370ba2ba64
codecpacka <ramprasad.r2600@gmail.com>
2021-05-29 16:14:31 +0530

    test

--- js/myscript.js
+++ js/myscript.js
@@ -9,6 +9,11 @@ app.controller("MyCtrl", ($scope) => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      $scope.all_data = data;
+      //   console.log("confirmed case are " + data.confirmed.value);
+      //   console.log("death cases are " + data.deaths.value);
+      //   console.log("recovered cases are " + data.recovered.value);
     });
+  $scope.ccase = data.confirmed.value;
+  $scope.rcase = data.recovered.value;
+  $scope.dcase = data.deaths.value;
 });
