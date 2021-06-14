56cb59924aaf4f68c731cca8595db264f0666507
BhargavSankaliya <sakaliyabhargav@gmail.com>
2021-06-14 17:50:58 +0530

    done all process

--- project/script.js
+++ project/script.js
@@ -34,15 +34,15 @@ const getColor = (ratio) => {
 
   const r = Math.ceil(
     parseInt(color1.substring(0, 2), 16) * ratio +
-      parseInt(color2.substring(0, 2), 16) * (1 - ratio)
+    parseInt(color2.substring(0, 2), 16) * (1 - ratio)
   );
   const g = Math.ceil(
     parseInt(color1.substring(2, 4), 16) * ratio +
-      parseInt(color2.substring(2, 4), 16) * (1 - ratio)
+    parseInt(color2.substring(2, 4), 16) * (1 - ratio)
   );
   const b = Math.ceil(
     parseInt(color1.substring(4, 6), 16) * ratio +
-      parseInt(color2.substring(4, 6), 16) * (1 - ratio)
+    parseInt(color2.substring(4, 6), 16) * (1 - ratio)
   );
 
   return "#" + getHex(r) + getHex(g) + getHex(b);
@@ -62,52 +62,52 @@ const getColor = (ratio) => {
 //   BEL3481: 1106992,
 //   BEL3482: 403599,
 // };\
-const data = {
-  AN: 128108,
-  UT: 17305,
-  CT: 925,
-  TG: 29208,
-  AP: 125108,
-  AR: 3753,
-  AS: 51403,
-  BR: 9627,
-  CH: 24895,
-  DH: 232,
-  DD: 232,
-  DL: 6731,
-  GA: 8216,
-  GJ: 20087,
-  HR: 9974,
-  HP: 9484,
-  JK: 28423,
-  JH: 6239,
-  KA: 268275,
-  KL: 167980,
-  LD: 1160,
-  MP: 11344,
-  MH: 188027,
-  MN: 9016,
-  ML: 5797,
-  MZ: 3097,
-  NL: 4819,
-  OR: 71071,
-  PY: 8783,
-  PB: 24454,
-  RJ: 21550,
-  SK: 4240,
-  TN: 257463,
-  TS: 29208,
-  TR: 6404,
-  UP: 19438,
-  UK: 17305,
-  WB: 44441,
-};
+// const data = {
+//   AN: 128108,
+//   UT: 17305,
+//   CT: 925,
+//   TG: 29208,
+//   AP: 125108,
+//   AR: 3753,
+//   AS: 51403,
+//   BR: 9627,
+//   CH: 24895,
+//   DH: 232,
+//   DD: 232,
+//   DL: 6731,
+//   GA: 8216,
+//   GJ: 20087,
+//   HR: 9974,
+//   HP: 9484,
+//   JK: 28423,
+//   JH: 6239,
+//   KA: 268275,
+//   KL: 167980,
+//   LD: 1160,
+//   MP: 11344,
+//   MH: 188027,
+//   MN: 9016,
+//   ML: 5797,
+//   MZ: 3097,
+//   NL: 4819,
+//   OR: 71071,
+//   PY: 8783,
+//   PB: 24454,
+//   RJ: 21550,
+//   SK: 4240,
+//   TN: 257463,
+//   TS: 29208,
+//   TR: 6404,
+//   UP: 19438,
+//   UK: 17305,
+//   WB: 44441,
+// };
 
 function main() {
-  let data2 = datam;
-  for (key in data2) {
-    console.log(data2[key]);
-  }
+  // let data2 = datam;
+  // for (key in data2) {
+  //   console.log(data2[key]);
+  // }
   // console.log(states);
   // onGeneratedRow(sates, data);
   fetch("india.svg")
@@ -119,42 +119,52 @@ function main() {
       const draw = SVG(image.slice(startOfSvg)).addTo("#map").size("100%");
 
       // get maximum value among the supplied data
-      const max = Math.max(...Object.values(data));
+      // const max = Math.max(...Object.values(data));
+      const data = Object.values(datam.state_wise);
+      const max = Math.max.apply(Math, data.map(function (o) { return o.active; }));
       console.log(max);
       for (const region of draw.find("path")) {
-        const regionValue = data[region.id()];
+        // const regionValue = data[region.id()];
+        const currentStateData = data.find(function (obj) { if (obj.statecode === region.id()) { return true } });
+        if (currentStateData && currentStateData.hasOwnProperty('active')) {
+          const regionValue = currentStateData.active;
+          const confirmed = currentStateData.confirmed;
+          const deaths = currentStateData.deaths;
+          const recovered = currentStateData.recovered;
 
-        if (isFinite(regionValue)) {
-          // color the region based on it's value with respect to the maximum
-          region.fill(getColor(regionValue / max));
+          if (isFinite(regionValue)) {
+            // color the region based on it's value with respect to the maximum
+            region.fill(getColor(regionValue / max));
 
-          // show region value as a label
-          draw
-            .text(`${region.attr("name")}`)
-            .font({
-              size: "0.65em",
-            })
-            .center(region.cx(), region.cy());
+            // show region value as a label
+            draw
+              .text(`${region.attr("name")}`)
+              .font({
+                size: "0.65em",
+              })
+              .center(region.cx(), region.cy());
+          }
+
+          // show region data when clicking on it
+          region.on("click", () => {
+            alert(`${region.attr("name")} (${region.id()}) :  Active Cases : ${regionValue} , Confirm Cases : ${regionValue} , Deaths Cases : ${deaths} , Recovered Cases: ${recovered}`);
+          });
+
+          region.on("mouseover", () => {
+            popperEl.innerHTML = `<b>${region.attr(
+              "name"
+            )} (${region.id()})</b><p style="color:orange"><b> Active Cases : ${regionValue} </b></p></b><p style="color:red"><b> Confirm Cases : ${regionValue} </b></p> <p style="color:yellow"><b>Deaths Cases : ${deaths} </b></p> <p style="color:green"><b>Recovered Cases: ${recovered}</b></p>`;
+            popperEl.style.visibility = "visible";
+            popperInstance = Popper.createPopper(region.node, popperEl, {
+              placement: "bottom",
+            });
+          });
+
+          region.on("mouseleave", () => {
+            popperEl.style.visibility = "hidden";
+          });
         }
 
-        // show region data when clicking on it
-        region.on("click", () => {
-          alert(`${region.attr("name")} (${region.id()}): ${regionValue}`);
-        });
-
-        region.on("mouseover", () => {
-          popperEl.innerHTML = `${region.attr(
-            "name"
-          )} (${region.id()})<b style="color:red"> active cases:</b><b> ${regionValue}</b>`;
-          popperEl.style.visibility = "visible";
-          popperInstance = Popper.createPopper(region.node, popperEl, {
-            placement: "bottom",
-          });
-        });
-
-        region.on("mouseleave", () => {
-          popperEl.style.visibility = "hidden";
-        });
       }
     });
 }
